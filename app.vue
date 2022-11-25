c875a7c0248a14cf2d88e4d2d5076c46943edaf8
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2022-11-24 19:50:35 -0500

    Initial Commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<NuxtWelcome />
+	</div>
+</template>
+
+<script setup>
+const tracks = useTracks();
+console.log(tracks);
+</script>
---
+++ composables/useTracks.js
@@ -0,0 +1,18 @@
+export default function useTracks() {
+	const tracks = import.meta.glob("/assets/music/*", { eager: true });
+
+	console.log(tracks);
+
+	const data = Object.values(tracks).map((t) => {
+		const track = t.default;
+		const title = track.match(/;(.+);/)?.[1];
+		const label = track.match(/!(.+)!/)?.[1];
+		const category = track.match(/\((.+)\)/)?.[1];
+		const subcategory = track.match(/\[(.+)\]/)?.[1];
+		const intensity = track.match(/\{(\d+)\}/)?.[1];
+
+		return { filepath: track, title, label, category, subcategory, intensity };
+	});
+
+	return data;
+}
